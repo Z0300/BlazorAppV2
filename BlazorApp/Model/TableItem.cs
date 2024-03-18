84a959b8cf556e4e910b63d80e0add90a39d3cbb
JP <rulljohnpatrick1496@gmail.com>
2024-03-18 23:06:53 +0800

    USER PAGES 1

---
+++ BlazorApp/Model/TableItem.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp.Model
+{
+    public class TableItem
+    {
+        public string Column1 { get; set; }
+        public string Column2 { get; set; }
+        public string Column3 { get; set; }
+        public string Column4 { get; set; }
+        public string Column5 { get; set; }
+
+        // Add more properties as needed
+        public bool Selected { get; set; }
+    }
+}
